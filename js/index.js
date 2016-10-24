6a5607cf7f3b5f6a08fd71d9738628973095e6aa
taingk <taingkn@gmail.com>
2016-10-24 16:16:52 +0200

    Init cookies

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function playsong() {
     return 0;
 }
 
-/* STATIC VARIABLES */ 
+/* STATIC VARIABLES */
 var GAMEBOARDWIDTH = $('#gameboard').width();
 var GAMEBOARDHEIGHT = $('#gameboard').height();
 var MOVESTEP = 15;
@@ -47,7 +47,7 @@ function moveBloc() {
     bloc1.right -= MOVESTEP;
     bloc1.left -= MOVESTEP;
     $('#bloc1').css('left', bloc1.left + 'px');
-    
+
     /* IF THE BLOC PASS THROUGH THE GAMEBOARD */
     if (bloc1.left < 0 - bloc1.width) {
         bloc1.right = GAMEBOARDWIDTH;
@@ -65,7 +65,7 @@ function moveBloc() {
                 console.log('Game over ! top');
                 clearInterval(move);
             }
-        } 
+        }
         /* ... AT THE LEFT */
         else {
             console.log('Game over ! right');
@@ -123,7 +123,7 @@ function keyFunction(evnt) {
             jump();
         break;
         case "DOWN":
-            clearInterval(move);          
+            clearInterval(move);
             $('#bloc1').css('left', GAMEBOARDWIDTH + 'px');
             move = setInterval(moveBloc, MOVE_DELAI);
         break;
