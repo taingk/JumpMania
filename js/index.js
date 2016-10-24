82fef4ef589849a310fa648568e9d39a7ec0ef20
Bastien <bastien@ngine.co>
2016-10-24 12:18:32 +0200

    First version

--- js/index.js
+++ js/index.js
@@ -6,12 +6,14 @@ function playsong() {
 
 var GAMEBOARDWIDTH = $('#gameboard').width();
 var GAMEBOARDHEIGHT = $('#gameboard').height();
-var MOVESTEP = 50;
+var MOVESTEP = 10;
 var LIFENB = 3;
 
 var player = {
     width : $('#player').width(),
     height  : $('#player').height(),
+    top : $('#player').position().top,
+    bottom : $('#player').position().top + $('#player').height(),
     right : $('#player').position().left + $('#player').width(),
     left : $('#player').position().left,
     life : LIFENB,
@@ -20,28 +22,18 @@ var player = {
 var bloc1 = {
     width : $('#bloc1').width(),
     height  : $('#bloc1').height(),
+    top : $('#bloc1').position().top,
+    bottom : $('#bloc1').position().top + $('#bloc1').height(),
     right : GAMEBOARDWIDTH,
     left : $('#bloc1').position().left,
 }
 
-var score = 0;
+var sky = 145;
+var ground = 370;
 
+var score = 0;
 var move;
 var jumping = false;
-
-//var left1;
-
-//var timerrrr;
-//var left1 = 1000; //parseInt($('#bloc1').css('left').slice(0, -2));
-//var top1 = 250; //parseInt($('#bloc1').css('top').slice(0, -2));
-//var height1 = 50; //parseInt($('#bloc1').css('height').slice(0, -2));
-//var left = 500; //parseInt($('#bloc').css('left').slice(0, -2));
-//var width = 100; //parseInt($('#bloc').css('width').slice(0, -2));
-//var topp = 200; //parseInt($('#bloc').css('top').slice(0, -2));
-//var heightt = 100; //parseInt($('#bloc').css('height').slice(0, -2));
-//var cassetoi;
-//var jumpp = false;
-//var score = 0;
 
 function moveBloc() {
 
@@ -56,12 +48,14 @@ function moveBloc() {
         bloc1.left = GAMEBOARDWIDTH - $('#bloc1').width();
 
         $('#bloc1').css('left', bloc1.left + 'px');
+        
         score ++;
+        $('#score').html(score);
     }
 
     if (bloc1.left <= player.right && bloc1.left >= player.left) {
         if (jumping) {
-            if (height1 >= (300 - topp - heightt)) {
+            if (bloc1.top <= player.bottom) {
                 console.log('Game over ! top');
                 clearInterval(move);
             }
@@ -70,56 +64,60 @@ function moveBloc() {
             clearInterval(move);
         }
     }
-    /*else if(left1 <= left){
-		score ++;
-		$('#score').html(score);
-	}*/
 }
 
-/*
+
 function jump() {
-    jumpp = true;
-    var bloc = parseInt($('#player').css('top').slice(0, -2));
-    var time = setInterval(up, 50);
+    jumping = true;
+
+    var time = setInterval(up, MOVESTEP);
     var timer;
 
     function up() {
-        $('#player').css('top', bloc - 50 + 'px');
-        bloc = bloc - 50;
-        topp = topp - 50;
-        if (bloc == 50) {
+        player.top -= MOVESTEP;
+        player.bottom -= MOVESTEP;
+
+        $('#player').css('top', player.top + 'px');
+        //bloc = bloc - 50;
+        //topp = topp - 50;
+        if (player.top < sky) {
             clearInterval(time);
             setTimeout(function() {
-                timer = setInterval(down, 50);
+                timer = setInterval(down, MOVESTEP);
             }, 150);
         }
     }
 
     function down() {
-        $('#player').css('top', bloc + 50 + 'px');
-        bloc = bloc + 50;
-        topp = topp + 50;
-        if (bloc == 200)
-            clearInterval(timer);
-        jumpp = false;
-    }
-}*/
+        player.top += MOVESTEP;
+        player.bottom += MOVESTEP;
 
-/*function keyFunction(evnt) {
+        $('#player').css('top', player.top + 'px');
+
+        if (player.bottom >= ground){
+
+            clearInterval(timer);
+
+            jumping = false;
+        }
+    }
+}
+
+function keyFunction(evnt) {
     switch (evnt) {
         case "UP":
             jump();
-            break;
+        break;
         case "DOWN":
             clearInterval(move);
-            $('#bloc1').css('left', '1000px');
-            //bloc1 = 1000;
-            //left1 = 1000;
-            move = setInterval(goLeft, 50);
-            break;
+            
+            $('#bloc1').css('left', GAMEBOARDWIDTH + 'px');
+
+            move = setInterval(moveBloc, MOVESTEP);
+        break;
     }
-}*/
+}
 
 var start = setTimeout(function() {
-    move = setInterval(moveBloc, 100);
+    move = setInterval(moveBloc, MOVESTEP);
 }, 2000);
